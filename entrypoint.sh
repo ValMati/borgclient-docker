7c1d3caf1e0eecf2c31ec0709880b847ed6e264a
ValMati <v.matilla@protonmail.com>
2021-08-21 11:31:59 +0200

    Add backup script

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,13 +3,16 @@
 BACKUP_DELAY=1
 CRONTAB_FILE='/etc/crontabs/root'
 
-current_minute=$(date +"%M")
 current_hour=$(date +"%H")
+current_minute=$(date +"%M")
 
-future_hour=$(( (${current_hour} + (${current_minute} + ${BACKUP_DELAY}) / 60) % 24 ))
+current_hour=${current_hour#"${current_hour%%[!0]*}"}
+current_minute=${current_minute#"${current_minute%%[!0]*}"}
+
+future_hour=$(( (${current_hour} + ((${current_minute} + ${BACKUP_DELAY}) / 60)) % 24 ))
 future_minute=$(( (${current_minute} + ${BACKUP_DELAY}) % 60 ))
 
-echo 'Adding task to crontab...'
-echo -e ${future_minute}'\t'${future_hour}'\t*\t*\t*\t/bin/backup_script.sh' 2>&1 | tee -a ${CRONTAB_FILE}
+echo 'Adding backup task to crontab...'
+echo -e ${future_minute}'\t'${future_hour}'\t*\t*\t*\t/bin/borg.sh' 2>&1 | tee -a ${CRONTAB_FILE}
 
 exec "$@"
