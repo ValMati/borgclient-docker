d5d355f204b4c45be73f4f701d9af4a60e91d7d4
ValMati <v.matilla@protonmail.com>
2021-08-22 11:30:31 +0200

    Generate ssh keys

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,19 @@
 #!/bin/sh
 
+HOST_KEYS_DIR='/root/.ssh'
 BACKUP_DELAY=5
 CRONTAB_FILE='/etc/crontabs/root'
+
+echo "> Configuring borg client"
+
+# Generate host keys if is necesary
+echo ">> Generating host keys if is necesary"
+if [ ! -f "${HOST_KEYS_DIR}/id_rsa" ]; then
+	ssh-keygen -f ${HOST_KEYS_DIR}/id_rsa -N '' -t rsa
+fi
+
+# Add bakup task to crontab
+echo '>> Adding backup task to crontab...'
 
 current_hour=$(date +"%H")
 current_minute=$(date +"%M")
@@ -12,7 +24,6 @@ current_minute=${current_minute#"${current_minute%%[!0]*}"}
 future_hour=$(( (${current_hour} + ((${current_minute} + ${BACKUP_DELAY}) / 60)) % 24 ))
 future_minute=$(( (${current_minute} + ${BACKUP_DELAY}) % 60 ))
 
-echo 'Adding backup task to crontab...'
 echo -e ${future_minute}'\t'${future_hour}'\t*\t*\t*\t/bin/borg.sh' 2>&1 | tee -a ${CRONTAB_FILE}
 
 exec "$@"
