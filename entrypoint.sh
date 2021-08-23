53a138271d05b456af6d903a593d9a87a3d77bf1
ValMati <v.matilla@protonmail.com>
2021-08-23 22:11:49 +0200

    Fix tabs lenght

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,14 +11,14 @@ echo "> Configuring borg client"
 # Generate host keys if is necesary
 echo ">> Generating host keys if is necesary"
 if [ ! -f "${HOST_KEYS_DIR}/id_rsa" ]; then
-        ssh-keygen -f ${HOST_KEYS_DIR}/id_rsa -N '' -t rsa
+    ssh-keygen -f ${HOST_KEYS_DIR}/id_rsa -N '' -t rsa
 fi
 
 # Check exclude file
 echo ">> Checking if exclude file exists"
 if [ ! -f ${EXCLUDE_FILE} ]; then
-        echo "Exclude file does not exists, creating..."
-        touch ${EXCLUDE_FILE}
+	echo "Exclude file does not exists, creating..."
+    touch ${EXCLUDE_FILE}
 fi
 
 # Add bakup task to crontab
@@ -36,12 +36,12 @@ future_minute=$(( (${current_minute} + ${BACKUP_DELAY}) % 60 ))
 borg_task=${future_minute}'\t'${future_hour}'\t*\t*\t*\t'${BORG_SCRIPT}
 grep=$(grep "${BORG_SCRIPT}" ${CRONTAB_FILE})
 if [ "${grep}" = "" ]; then
-        echo "Adding..."
-        echo -e "${borg_task}" 2>&1 | tee -a ${CRONTAB_FILE}
+	echo "Adding..."
+    echo -e "${borg_task}" 2>&1 | tee -a ${CRONTAB_FILE}
 else
-        echo "Updating..."
-        echo -e "${borg_task}"
-        sed -i '/borg.sh/c'${borg_task} ${CRONTAB_FILE}
+    echo "Updating..."
+    echo -e "${borg_task}"
+    sed -i '/borg.sh/c'${borg_task} ${CRONTAB_FILE}
 fi
 
 exec "$@"
